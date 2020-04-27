d17974353f3f36cec8c194e99423b0734baf43d4
iurifulgidi <iurifulgidi@hotmail.com>
2020-04-27 23:46:18 +0200

    bozza di interfaccia e implementazione del pattern Fire and forget

---
+++ FireForget/FireForget/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibraryTask;
+
+namespace FireForget
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        CancellationTokenSource cts;
+
+        private void Btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            cts = new CancellationTokenSource();
+
+            Worker wrkr = new Worker(10,1000,cts );
+
+            wrkr.CountDown();
+
+            MessageBox.Show("hey yo wassup, i don't give a shit about the other guy");
+        }
+
+        private void Btn_stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (cts != null)
+                cts.Cancel();
+        }
+    }
+}
---
+++ FireForget/LibraryTask/worker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraryTask
+{
+    public class Worker
+    {
+        CancellationTokenSource Cts;
+        int Max;
+        int Delay;
+
+        public Worker(int max,int delay,CancellationTokenSource cts)
+        {
+            Max = max;
+            Delay = delay;
+            Cts = cts;
+        }
+
+        public void CountDown()
+        {
+            Task.Factory.StartNew(Count);
+        }
+
+        private void Count()
+        {
+            for (int i = Max; i > 0; i--)
+            {
+                if (Cts.IsCancellationRequested)
+                    break;
+
+                Thread.Sleep(Delay);
+            }
+        }
+    }
+}
